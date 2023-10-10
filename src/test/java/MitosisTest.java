3c27f5d9c3c58b6d56554ac5ad318f3dae4936b0
Dan The Man <danqdon@gmail.com>
2023-10-10 17:28:57 +0100

    Added TODOs for tomorrow:)

--- src/test/java/MitosisTest.java
+++ src/test/java/MitosisTest.java
@@ -21,14 +21,17 @@ public class MitosisTest {
         DNAStrand dnaStrand = dnaDeserializer.deserialize("genome.txt");
         GenomeOrganizer organizer = new GenomeOrganizer();
         List<Chromatid> chromatidList = organizer.organizeIntoChromatids(dnaStrand);
+        DNAPolymerase dnaPolymerase = new DNAPolymerase();
+        //TODO añadir cromatida complementaria y luego meter ambas en el cromosoma
         List<Chromosome> chromosomes = new ArrayList<>();
         for (Chromatid chromatid : chromatidList)
             chromosomes.add(new Chromosome(chromatid, chromatid.getId()));
-        DNAPolymerase dnaPolymerase = new DNAPolymerase();
-        Cell cell = new Cell(chromosomes, dnaPolymerase);
-        cell.replicateDNA();
+
+        Cell cell = new Cell(chromosomes, dnaPolymerase); //TODO adaptar la clase para que admita que un cariotipo
+                                                          // tenga un par de cromosomas por elemento
+        cell.replicateDNA(); //TODO cambiar el replicate para que duplique en 2 cromosomas, y no haga complementario
         Microtubule microtubule = new Microtubule(dnaPolymerase);
-        List<Cell> daughterCells = microtubule.divideCell(cell);
+        List<Cell> daughterCells = microtubule.divideCell(cell); //TODO separa cada elemento del cariotipo y crea dos celulas con cromosomas individuales
         Cell daughterCell1 = daughterCells.get(0);
         Cell daughterCell2 = daughterCells.get(1);
         DNAStrandCellSerializer genomeSerializer = new DNAStrandCellSerializer();
