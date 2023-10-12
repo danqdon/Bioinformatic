2d73118d81655f1689eda87eb443d83cc952443b
Dan The Man <danqdon@gmail.com>
2023-10-12 23:34:54 +0100

    Microtubule method fixed, should be final except for possible refactoring

--- src/Deserializers/TxtStringDeserializer.java
+++ src/Deserializers/TxtStringDeserializer.java
@@ -3,7 +3,6 @@ package Deserializers;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.stream.IntStream;
 
 public class TxtStringDeserializer {
 
@@ -11,8 +10,8 @@ public class TxtStringDeserializer {
         String expected = Files.readString(Path.of(file)).toUpperCase();
 
         int length = expected.length();
-        int trimFactor = length%(23*3);
-        length -=trimFactor;
+        int trimFactor = length % (23 * 3);
+        length -= trimFactor;
         expected = expected.substring(0, length);
         return expected;
     }
