5c4b737d3928d1e8c62a89debd4ce25631253129
Dan The Man <danqdon@gmail.com>
2023-10-13 10:07:42 +0100

    Optimized ProteiniSynthesis deserialization

--- src/test/java/ProteinSynthesisTest.java
+++ src/test/java/ProteinSynthesisTest.java
@@ -1,29 +1,20 @@
 import Biomolecules.DNAStrand;
+import Deserializers.TxtDNADeserializer;
 import ProteinSynthesis.Exceptions.GeneSequenceNotFoundException;
 import ProteinSynthesis.*;
 import Serializers.StringDNASerializer;
 import Serializers.StringPeptideSerializer;
 import Serializers.StringRNASerializer;
-import Deserializers.TxtDNADeserializer;
 import org.junit.Test;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class ProteinSynthesisTest {
 
     @Test
     public void name() throws GeneSequenceNotFoundException {
-        String sampleGenomeStrand = readGenomeFromFile("genome.txt");
-        String sampleGeneStrand = readGenomeFromFile("gene.txt");
-
-        if (sampleGenomeStrand == null || sampleGeneStrand == null) {
-            return;
-        }
 
         TxtDNADeserializer dnaDeserializer = new TxtDNADeserializer();
-        DNAStrand genome = dnaDeserializer.deserialize(sampleGenomeStrand);
-        DNAStrand gene = dnaDeserializer.deserialize(sampleGeneStrand);
+        DNAStrand genome = dnaDeserializer.deserialize("genome.txt");
+        DNAStrand gene = dnaDeserializer.deserialize("gene.txt");
         GeneLocator locator = new GeneLocator(genome);
         DNAStrand dnaStrand = locator.locate(gene);
         StringDNASerializer dnaSerializer = new StringDNASerializer();
@@ -40,15 +31,5 @@ public class ProteinSynthesisTest {
         System.out.println("Genome: " + dnaSerializer.serialize(genome));
         System.out.println("Messenger RNA Strand: " + rnaSerializer.serialize(messengerRnaStrand));
         System.out.println("Peptidic Chain: " + peptideSerializer.serialize(peptidicChain));
-    }
-
-    private String readGenomeFromFile(String fileName) {
-        try {
-            String content = new String(Files.readAllBytes(Paths.get(fileName))).toUpperCase();
-            return content.replaceAll("\\s+", "");  // Removing any whitespaces
-        } catch (Exception e) {
-            System.err.println("Error reading the genome file: " + e.getMessage());
-            return null;
-        }
     }
 }
